120a7eeeb199278c111d598cac2f1658036bb956
Horsty80 <cy.hue80@gmail.com>
2016-05-21 12:58:14 +0200

    mise en ligne du premier fichier

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    Coucou ceci est du html
+    <?php
+    echo "ceci est un écho c'est donc du php";
+    $compteur = 1;
+    while($compteur < 10){
+        echo $compteur;
+        echo "<br />";
+        $compteur++;
+    }
+    ?>
+</html>
